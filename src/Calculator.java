0fd08118af90c76ee2985d3fb9c5108829f5db67
imjinbro <jinbro@likelion.org>
2017-12-14 13:12:31 +0900

    계산기 프로그램 실행시키고 싶은 만큼 계산할 수 있도록 함, 종료키 만

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,70 +10,84 @@ import java.util.Scanner;
  	- String은 처음에 같은 주소값인지 비교함
  	- 같은 주소값을 가지고 있지않다면 String인지 체크(instanceof)
  	- String이라면 문자열 각 인덱스 문자를 비교함 : 같은 문자열이라면 true  	
-     
- 	[현재 코드 문제점]
- 	1. 입력 숫자가 여러개 일 때
- 	2. 연산자 분기 : if ~ else 떡
- 	
+         
+    [개선]
+    1. 실행시키고 싶은 만큼 계산할 수 있음 : while - 탈출문  
+ 	2. 프린터가 어떤 것이냐에 따라서 알아서 프린트되도록 : 입력값 물을 때도
+ 	3. 계속해서 계산할 수 있도록 함
+ 	- 첫번째 값 입력 후 사칙연산 입력 에러인 경우 첫번째 값 유지
+  	
  */
 public class Calculator {		
-	public static void main(String[] args){	
-		Scanner sc = new Scanner(System.in); //Stream은 byte, char 단위 통
-		System.out.println("첫번째 값 입력 : ");
-		int num1 = sc.nextInt();
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		CalcPrinter printer = ConsoleCalcPrinter.getInstance();
+		Calculator calculator = new Calculator(ConsoleCalcPrinter.getInstance());
 		
-		System.out.println("사칙연산 기호 : ");
-		String symbol = sc.next();
+		printer.print("숫자를 입력해주세요");			
+		int result = scanner.nextInt();		
+		
+		while(true) {
+			printer.print("사칙 연산 기호 입력해주세요(종료하려면 q)");
+			String symbol = scanner.next();
 			
-		System.out.println("두번째 값 입력 : ");
-		int num2 = sc.nextInt();
-	
-		Calculator calculator = new Calculator(ConsoleCalcPrinter.getInstance());		
-		calculator.calc(symbol, num1, num2);
+			if(symbol.equals("q")) {
+				scanner.close();
+				calculator = null;	
+				printer.print("계산기 종료 - 최종결과 : " + result);
+				break;
+			}
+									
+			printer.print("숫자를 입력해주세요");
+			int num2 = scanner.nextInt();
+			
+			result = calculator.calc(symbol, result, num2);
+			calculator.printResult(result);
+		}
 		
 	}
+	
+	
 	private CalcPrinter printer;
 	
 	public Calculator(CalcPrinter printer) {
 		this.printer = printer;
 	}
 	
-	private void print(int result) {
+	private void printResult(int result) {
 		printer.print(result);
 	}
 	
-	private void print(String msg) {
-		printer.print(msg);
-	}
 	
-	public void calc(String symbol, int num1, int num2) {
+	public int calc(String symbol, int num1, int num2) {		
 		if(symbol.equals("+")) {
-			plus(num1, num2);
+			return plus(num1, num2);
 		} else if(symbol.equals("-")) {
-			minus(num1, num2);
+			return minus(num1, num2);
 		} else if(symbol.equals("*")) {
-			multiply(num1, num2);
+			return multiply(num1, num2);
 		} else if(symbol.equals("/")) {
-			divide(num1, num2);
+			return divide(num1, num2);
 		} else {
-			print("유효하지않은 기호입니다");
-		}
+			printer.print("유효하지않은 기호입니다");
+			return num1;
+		}				
 	}
 	
-	public void plus(int num1, int num2) {	
-		print(num1+num2);
+	public int plus(int num1, int num2) {	
+		return num1+num2;
 	}
 	
-	public void minus(int num1, int num2) {		
-		print(num1-num2);
+	public int minus(int num1, int num2) {		
+		return num1-num2;
 	}
 	
-	public void multiply(int num1, int num2) {		
-		print(num1*num2);
+	public int multiply(int num1, int num2) {		
+		return num1*num2;
 	}
 	
-	public void divide(int num1, int num2) {		
-		print(num1/num2);
+	public int divide(int num1, int num2) {		
+		return num1/num2;
 	}
 	
 }
